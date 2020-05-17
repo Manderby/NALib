4c2bd328801b3920cd713f223c5201eee90f954c
Tobias Stamm <manderby@manderby.com>
2020-05-17 10:28:26 +0200

    Cleaning up VisualStudio solutions and projects

--- code/HelloWorldGUI/src/main.c
+++ code/HelloWorldGUI/src/main.c
@@ -1,19 +1,35 @@
 
+// To use the GUI implementation of NALib, you need to configure it. You can
+// do this by defining the preprocessor macro NA_COMPILE_GUI to be 1.
+//
+// You can define NA_COMPILE_GUI either directly in NAConfiguration.h or add
+// a preprocessor macro in your project settings. But the preferred ways are:
+//
+// XCode: Add a preprocessor macro to your solution settings. Add the Cocoa
+// framework (and in this example the OpenGL framework) to your linked
+// libraries.
+//
+// VisualStudio: Provide a property sheet named NAConfiguration.props right
+// next to the solution (go see the example of this solution). Add the
+// following line using a text editor to your project (go see this project):
+// <Import Project="$(SolutionDir)\NAConfiguration.props" Condition="Exists('$(SolutionDir)\NAConfiguration.props')" />
+// It is proposed to add this line right after the line which imports the
+// property sheet "Microsoft.Cpp.Default.props". The NALib project will
+// include NAConfiguration.props automatically.
+
+
+
 // To be able to include header files from NALib, you need to either add the
 // src directory of NALib as an additional project source path, or you can
 // help yourself using the following two lines. Just provide the correct
 // relative path to the root directory of the NALib source:
 
-#define GUITRYOUT_STRINGIFY(A) #A
-#define GUITRYOUT_NALIB_PATH(file) GUITRYOUT_STRINGIFY(../../NALib/src/file)
+#define HELLOWORLDGUI_STRINGIFY(A) #A
+#define HELLOWORLDGUI_NALIB_PATH(file) HELLOWORLDGUI_STRINGIFY(../../NALib/src/file)
 
-// This allows you to reference any NALib file using for example this:
-// #include GUITRYOUT_NALIB_PATH(NABase.h)
-
-
-#include GUITRYOUT_NALIB_PATH(NABase.h)
-#include GUITRYOUT_NALIB_PATH(NAUI.h)
-#include GUITRYOUT_NALIB_PATH(NATranslator.h)
+#include HELLOWORLDGUI_NALIB_PATH(NABase.h)
+#include HELLOWORLDGUI_NALIB_PATH(NAUI.h)
+#include HELLOWORLDGUI_NALIB_PATH(NATranslator.h)
 #include "translationIDs.h"
 
 NAInt trFinanceGroup;
@@ -21,11 +37,12 @@ NAInt trGeneralGroup;
 
 NABool reshapeWindow(NAReaction reaction){
   NA_UNUSED(reaction);
-  printf("Reshape" NA_NL);
+  // commented out as this just fills the console with the same line.
+  //printf("Reshape" NA_NL);
   return NA_TRUE;
 }
 
-NABool cubPressWindowKey(NAReaction reaction){
+NABool keyPressed(NAReaction reaction){
   NAKeyboardStatus keyStatus;
   NAUIKeyCode key;
 
@@ -37,27 +54,48 @@ NABool cubPressWindowKey(NAReaction reaction){
   return NA_TRUE;
 }
 
-NABool cubReleaseWindowKey(NAReaction reaction){
+NABool keyReleased(NAReaction reaction){
   NA_UNUSED(reaction);
   printf("Key Release" NA_NL);
   return NA_TRUE;
 }
 
-NABool cubMoveWindowMouse(NAReaction reaction){
+NABool mouseMoved(NAReaction reaction){
   NA_UNUSED(reaction);
-  printf("Mouse Move" NA_NL);
+  // commented out as this just fills the console with the same line.
+  //printf("Mouse Move" NA_NL);
   return NA_TRUE;
 }
 
-NABool cubEnterWindowMouse(NAReaction reaction){
+NABool mouseEntered(NAReaction reaction){
   NA_UNUSED(reaction);
   printf("Mouse Enter" NA_NL);
   return NA_TRUE;
 }
 
-NABool cubExitWindowMouse(NAReaction reaction){
+NABool mouseExited(NAReaction reaction){
   NA_UNUSED(reaction);
   printf("Mouse exit" NA_NL);
+  return NA_TRUE;
+}
+
+NABool buttonPressed(NAReaction reaction){
+  NA_UNUSED(reaction);
+  printf("Oh, do it again." NA_NL);
+  return NA_TRUE;
+}
+
+NABool checkBoxPressed(NAReaction reaction){
+  NA_UNUSED(reaction);
+  NABool state = naGetCheckBoxState(reaction.uielement);
+  printf("Checked. Status now is %d." NA_NL, (int)state);
+  return NA_TRUE;
+}
+
+NABool radioPressed(NAReaction reaction){
+  NA_UNUSED(reaction);
+  NABool state = naGetRadioState(reaction.uielement);
+  printf("Checked. Status of radio 0x%x now is %d." NA_NL, (int)reaction.uielement, (int)state);
   return NA_TRUE;
 }
 
@@ -81,7 +119,8 @@ void poststartup(void* arg){
   NAButton* button;
   NACheckBox* checkbox;
   NALabel* label;
-  NARadio* radio;
+  NARadio* radio1;
+  NARadio* radio2;
   NATextBox* textbox;
   NATextField* textfield;
   NA_UNUSED(arg);
@@ -97,14 +136,16 @@ void poststartup(void* arg){
 
 //  printf("%d" NA_NL, (int)naGetLanguageCode("deu"));
 
-  windowrect = naMakeRectS(20, 20, 400, 300);
+  printf(NA_NL "===========" NA_NL);
+
+  windowrect = naMakeRectS(120, 20, 400, 300);
   window = naNewWindow("Wurst", windowrect, NA_TRUE, 0);
   naAddUIReaction(window, NA_UI_COMMAND_RESHAPE,       reshapeWindow, NA_NULL);
-  naAddUIReaction(window, NA_UI_COMMAND_KEYDOWN,       cubPressWindowKey, NA_NULL);
-  naAddUIReaction(window, NA_UI_COMMAND_KEYUP,         cubReleaseWindowKey, NA_NULL);
-  naAddUIReaction(window, NA_UI_COMMAND_MOUSE_MOVED,   cubMoveWindowMouse, NA_NULL);
-  naAddUIReaction(window, NA_UI_COMMAND_MOUSE_ENTERED, cubEnterWindowMouse, NA_NULL);
-  naAddUIReaction(window, NA_UI_COMMAND_MOUSE_EXITED,  cubExitWindowMouse, NA_NULL);
+  naAddUIReaction(window, NA_UI_COMMAND_KEYDOWN,       keyPressed, NA_NULL);
+  naAddUIReaction(window, NA_UI_COMMAND_KEYUP,         keyReleased, NA_NULL);
+  naAddUIReaction(window, NA_UI_COMMAND_MOUSE_MOVED,   mouseMoved, NA_NULL);
+  naAddUIReaction(window, NA_UI_COMMAND_MOUSE_ENTERED, mouseEntered, NA_NULL);
+  naAddUIReaction(window, NA_UI_COMMAND_MOUSE_EXITED,  mouseExited, NA_NULL);
 
 //  NAOpenGLSpace* openglspace = naNewOpenGLSpace(window, windowrect.size, initOpenGLSpace, window);
 //  naSetWindowContentSpace(window, openglspace);
@@ -113,14 +154,25 @@ void poststartup(void* arg){
 
   button = naNewPushButton("PushButton asdf", naMakeSize(200., 20.));
   naAddSpaceChild(contentSpace, button, naMakePos(20., 20.));
+  naAddUIReaction(button, NA_UI_COMMAND_PRESSED, buttonPressed, NA_NULL);
+
   checkbox = naNewCheckBox("Checkbox asdf", naMakeSize(200., 20.));
   naAddSpaceChild(contentSpace, checkbox, naMakePos(20., 40.));
+  naAddUIReaction(checkbox, NA_UI_COMMAND_PRESSED, checkBoxPressed, NA_NULL);
+
   label = naNewLabel("Label asdf", naMakeSize(200., 20.));
   naAddSpaceChild(contentSpace, label, naMakePos(20., 60.));
-  radio = naNewRadio("Radio asdf", naMakeSize(200., 20.));
-  naAddSpaceChild(contentSpace, radio, naMakePos(20., 80.));
+  
+  radio1 = naNewRadio("Radio 1", naMakeSize(200., 20.));
+  radio2 = naNewRadio("Radio 2", naMakeSize(200., 20.));
+  naAddSpaceChild(contentSpace, radio1, naMakePos(20., 80.));
+  naAddSpaceChild(contentSpace, radio2, naMakePos(220., 80.));
+  naAddUIReaction(radio1, NA_UI_COMMAND_PRESSED, radioPressed, NA_NULL);
+  naAddUIReaction(radio2, NA_UI_COMMAND_PRESSED, radioPressed, NA_NULL);
+
   textbox = naNewTextBox(naMakeSize(200., 40.));
   naAddSpaceChild(contentSpace, textbox, naMakePos(20., 100.));
+  
   textfield = naNewTextField(naMakeSize(200., 20.));
   naAddSpaceChild(contentSpace, textfield, naMakePos(20., 140.));
 
