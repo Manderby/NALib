813000150b4803809a2c75ec960f18b046b9b8bd
Tobias Stamm <manderby@manderby.com>
2024-05-11 11:02:51 +0200

    Fixing error on macOS

--- code/NALib/src/NAApp/macOS/NACocoa.m
+++ code/NALib/src/NAApp/macOS/NACocoa.m
@@ -41,8 +41,6 @@
 #include "UI/NAUICocoaInterfaces.h"
 #include "UI/NAApplicationII.h"
 
-NAWINAPIColor* naGetWINAPISpaceBackgroundColor(const NAWINAPISpace* winapiSpace);
-
 #include "UI/NAButtonII.h"
 #include "UI/NACheckBoxII.h"
 #include "UI/NAImageSpaceII.h"
