9ed778a25c045cbd903808bbf6a75426630caa1b
Tobias Stamm <manderby@manderby.com>
2024-09-14 20:41:34 +0200

    make it compile on windows again

--- code/NALib/src/NAApp/winAPI/UIElements/NAWINAPIUIElementII.h
+++ code/NALib/src/NAApp/winAPI/UIElements/NAWINAPIUIElementII.h
@@ -6,6 +6,7 @@
 
 // todo: find a faster way. Hash perhaps or something else.
 NA_HDEF void* na_GetUINALibEquivalent(void* nativePtr) {
+
   NA_UIElement* retelem = NA_NULL;
   NAListIterator iter = naMakeListMutator(&na_App->uiElements);
   while(naIterateList(&iter)) {
@@ -16,5 +17,20 @@ NA_HDEF void* na_GetUINALibEquivalent(void* nativePtr) {
     }
   }
   naClearListIterator(&iter);
+
+  //retelem = (NA_UIElement*)GetWindowLongPtr(nativePtr, GWLP_USERDATA);
+
   return retelem;
+}
+
+
+
+NA_HDEF void na_InitSystemUIElement(NA_UIElement* uiElement, void* nativePtr) {
+  SetWindowLongPtr(nativePtr, GWLP_USERDATA, (LONG_PTR)uiElement);
+}
+
+
+
+NA_HAPI void na_ClearSystemUIElement(void* nativePtr) {
+  SetWindowLongPtr(nativePtr, GWLP_USERDATA, (LONG_PTR)NA_NULL);
 }
